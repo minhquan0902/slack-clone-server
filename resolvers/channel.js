c2e32367adf49d1dd304983bb0de38f0e030f5d6
minhquan0902 <minhquan0902@gmail.com>
2020-12-29 15:37:14 +0800

    Add Channel Form with Formik and Updating React Apollo Cache

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -1,13 +1,22 @@
+/* eslint-disable quotes */
+import formatErrors from "../formatErrors";
+
 export default {
   Mutation: {
     createChannel: async (parent, args, { models }) => {
       try {
-        await models.Channel.create(args);
-        return true;
+        const channel = await models.Channel.create(args);
+        return {
+          ok: true,
+          channel,
+        };
       } catch (err) {
         // eslint-disable-next-line no-console
         console.log(err);
-        return false;
+        return {
+          ok: false,
+          errors: formatErrors(err),
+        };
       }
     },
   },
