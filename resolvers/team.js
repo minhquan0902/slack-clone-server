0a669dffa2b068c8974576d105fa5410e3ffa756
minhquan0902 <minhquan0902@gmail.com>
2021-01-01 16:28:24 +0800

    Querying Many to Many Relationship with Sequelize

--- resolvers/team.js
+++ resolvers/team.js
@@ -12,6 +12,20 @@ export default {
       async (parent, args, { models, user }) =>
         models.Team.findAll({ where: { owner: user.id } }, { raw: true })
     ),
+    inviteTeams: requiresAuth.createResolver(
+      async (parent, args, { models, user }) =>
+        models.Team.findAll(
+          {
+            include: [
+              {
+                model: models.User,
+                where: { id: user.id },
+              },
+            ],
+          },
+          { raw: true }
+        )
+    ),
   },
   Mutation: {
     addTeamMember: requiresAuth.createResolver(
