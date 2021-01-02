556146b142701a1a76488ea27e4c7b6a4b8fddc2
minhquan0902 <minhquan0902@gmail.com>
2021-01-03 01:57:44 +0800

    implement messages query

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,18 +1,35 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable function-paren-newline */
+/* eslint-disable quotes */
+import requiresAuth from "../permission";
+
 export default {
+  Message: {
+    user: ({ userId }, args, { models }) =>
+      models.User.findOne({ where: { id: userId } }, { raw: true }),
+  },
   Query: {
     // eslint-disable-next-line no-unused-vars
-    messages: async (parent, args, { models, user }) => [],
+    messages: requiresAuth.createResolver(
+      async (parent, { channelId }, { models }) =>
+        models.Message.findAll(
+          { order: [["created_at", "ASC"]], where: { channelId } },
+          { raw: true }
+        )
+    ),
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(err);
-        return false;
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Message.create({ ...args, userId: user.id });
+          return true;
+        } catch (err) {
+          // eslint-disable-next-line no-console
+          console.log(err);
+          return false;
+        }
       }
-    },
+    ),
   },
 };
