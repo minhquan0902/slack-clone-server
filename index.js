536d299ecd97481e73da3fb2553dcfa54be8f97b
minhquan0902 <minhquan0902@gmail.com>
2021-01-03 02:35:38 +0800

    Graphql Subscriptions Setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-new */
+/* eslint-disable semi */
 /* eslint-disable arrow-parens */
 /* eslint-disable function-paren-newline */
 /* eslint-disable comma-dangle */
@@ -10,6 +12,9 @@ import path from "path";
 import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
 import models from "./models";
 import { refreshTokens } from "./auth";
@@ -78,6 +83,20 @@ app.use(
 
 app.use("/graphiql", graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
+const server = createServer(app);
+
 models.sequelize.sync({}).then(() => {
-  app.listen(8081);
+  server.listen(8081, () => {
+    new SubscriptionServer(
+      {
+        execute,
+        subscribe,
+        schema,
+      },
+      {
+        server,
+        path: "/subscriptions",
+      }
+    );
+  });
 });
